f5c1298ca8fc88f812184057471ebdf01dd225ab
Lou Mauriege <lou.mauriege@laposte.net>
2022-03-30 07:53:59 +0200

    Mise en page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var titres = document.getElementsByClassName("circulaire");
+var titres = document.getElementsByTagName("h2");
     for(var i= 0; i < titres.length; i++){
         new CircleType(titres[i]);
     }
-//new CircleType(document.getElementById('formation'));
